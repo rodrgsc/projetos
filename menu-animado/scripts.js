9dbfc79fb91fa2da32439a3f37a5cd47ec84eb45
rodrgsc <rodrgcs2@gmail.com>
2024-09-21 13:03:01 -0300

    Menu 1.0

---
+++ menu-animado/scripts.js
@@ -0,0 +1,21 @@
+const botao = document.querySelector(".botao-menu");
+const menuLateral = document.querySelector(".menu-lateral");
+const conteudo = document.querySelector(".conteudo");
+const background = document.querySelector(".background");
+
+botao.addEventListener("click", () => {
+  menuLateral.classList.toggle("ativo");
+  botao.classList.toggle("ativo");
+  conteudo.classList.toggle("ativo");
+  background.classList.toggle("ativo");
+  document.body.style.background = menuLateral.classList.contains("ativo")
+    ? "#34495e"
+    : "#ecf0f1";
+});
+background.addEventListener("click", () => {
+  menuLateral.classList.remove("ativo");
+  botao.classList.remove("ativo");
+  conteudo.classList.remove("ativo");
+  background.classList.remove("ativo");
+  document.body.style.background = "#ecf0f1";
+});
