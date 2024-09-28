9273e5d4dfe9a7d33ca99ba69c72e4e7def1eec7
rodrgsc <rodrgcs2@gmail.com>
2024-09-27 21:29:00 -0300

    First Commit

---
+++ TDD-testes-unitarios/Calculadora/Calculadora.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TDDTestesUnitarios.Calculadora
+{
+	public class Calculadora
+	{
+		public void somar(int numero1, int numero2)
+		{
+			return 0;
+		}
+		public void subtrair(int numero1, int numero2)
+		{
+			return 0;
+		}
+		public void multiplicar(int numero1, int numero2)
+		{
+			return 0;
+		}
+		public void dividir(int numero1, int numero2)
+		{
+			return 0;
+		}
+
+		public List<string> historico()
+		{
+			return new List<string>;
+		}
+	}
+}
